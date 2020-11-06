000c06c24b8849b86dbefc534a8862bbdf9ca139
ian-chiu <ianchiu41@gmail.com>
2020-11-07 00:49:52 +0800

    show mouse cursor location and successfully add boost-polygon library to cmake

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,30 +11,38 @@ using poly_t = std::vector<sf::Vector2f>;
 int main()
 {
 
-    /// ----------------READ DATAS-----------------
+    /// ----------------SETTINGS-----------------
+    // load fonts
+    sf::Font font;
+    if (!font.loadFromFile("../resource/arial.ttf"))
+    {
+        std::cerr << "Cannot load font!\n";
+        return -1;
+    }
+
     std::vector<rect_t> rects{};
     std::vector<poly_t> polys{};
 
     // read the data from rects.txt
-    std::ifstream rects_file{ "../data/rects.txt" };
-    if (!rects_file) {
-        std::cerr << "Cannot open rects.txt";
-        return -1;
-    }
-    while (rects_file) {
-        std::string strInput;
-        rects_file >> strInput;
-        if (strInput == "RECT") {
-            rect_t rect{};
-            while (rects_file >> strInput)  {
-                if (strInput == ";") {
-                    break;
-                }
-                rect.push_back(stof(strInput));
-            }
-            rects.push_back(rect);
-        }
-    }
+    // std::ifstream rects_file{ "../data/rects.txt" };
+    // if (!rects_file) {
+    //     std::cerr << "Cannot open rects.txt";
+    //     return -1;
+    // }
+    // while (rects_file) {
+    //     std::string strInput;
+    //     rects_file >> strInput;
+    //     if (strInput == "RECT") {
+    //         rect_t rect{};
+    //         while (rects_file >> strInput)  {
+    //             if (strInput == ";") {
+    //                 break;
+    //             }
+    //             rect.push_back(stof(strInput));
+    //         }
+    //         rects.push_back(rect);
+    //     }
+    // }
 
     // read polygons (read the data from input.txt)
     std::ifstream input_file{"../data/input.txt"};
@@ -80,9 +88,21 @@ int main()
 
         window.clear(sf::Color::Black);
 
+        // lambda function used to transform the position to plotPosition
         auto plotPos = [nScr_h](float x, float y) {
             return sf::Vector2f(x, nScr_h - y);
         };
+
+        // print mouse cursor position
+        sf::Vector2i localPosition = sf::Mouse::getPosition(window);
+        sf::Vector2f mousePlotPos = plotPos(localPosition.x, localPosition.y);
+        sf::Text text;
+        text.setFont(font);
+        text.setCharacterSize(20);
+        text.setFillColor(sf::Color::White);
+
+        text.setString(std::to_string((int)mousePlotPos.x) + ", " + std::to_string((int)mousePlotPos.y));
+        window.draw(text);
 
         for (const auto &rect : rects) {
             sf::RectangleShape rectShape(sf::Vector2f(rect[2] - rect[0], rect[3] - rect[1]));
@@ -95,13 +115,14 @@ int main()
 
         for (const auto &poly : polys) {
             sf::ConvexShape convex;
-            convex.setFillColor(sf::Color::Blue);
+            convex.setFillColor(sf::Color::Black);
             convex.setOutlineColor(sf::Color::White);
             convex.setOutlineThickness(-1.f);
             convex.setPointCount(poly.size());
             for (int i = 0; i < poly.size(); ++i) {
                 convex.setPoint(i, plotPos(poly[i].x, poly[i].y));
             }
+            
             window.draw(convex);
         }
 
