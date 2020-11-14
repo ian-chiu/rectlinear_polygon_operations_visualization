1466880671aa172d987dbfa007b23c6260f2db4f
ian-chiu <ianchiu41@gmail.com>
2020-11-14 22:49:02 +0800

    abstraction: Solution.h Solution.cpp

---
+++ include/App.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "App.fwd.h"
+#include "Solution.fwd.h"
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <deque>
+#include "Solution.h"
+#include "cmake_variables.h"
+#include "gtl_poly_types.h"
+
+class App
+{
+public:
+    App() = delete;
+    App(sf::RenderWindow &window, int &win_width, int &win_height);
+    void render(const Solution &solution);
+
+    sf::Text hint_text;
+    sf::Text mouse_text;
+    bool split_mode{ false };
+
+private:
+    sf::RenderWindow &window;
+    int &win_width;
+    int &win_height;
+    sf::Font font;
+    inline sf::Vector2f plotPos(float x, float y) const;
+    void draw_rectangles(const std::vector<Rect> &rects) const;
+    void draw_polygon_set(const PolygonSet &ps) const;
+};
---
+++ include/Solution.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "App.fwd.h"
+#include "Solution.fwd.h"
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <deque>
+#include "App.h"
+#include "gtl_poly_types.h"
+
+class Solution
+{
+    friend class App;
+
+public:
+    Solution() = delete;
+    Solution(std::string infile, std::string outfile);
+    ~Solution();
+
+    void read_operations();
+    void execute_operation(std::string oper, App &app);
+    void execute_split();
+    std::deque<std::string> copy_operations();
+    std::string get_split_method();
+
+private:
+    std::ifstream input_file;
+    std::ofstream output_file;
+    std::deque<std::string> operations{};
+    std::vector<Rect> output_rects{};
+    PolygonSet polygon_set{};
+    std::string token;
+    std::istringstream iss;
+    std::string split_method{ "SO" };
+};
